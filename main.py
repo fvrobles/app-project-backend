d9bcf6b91a03ce2fbbc4daaa47b7df2877071cb8
FernandoVR <fernando.vr.dev@gmail.com>
2024-05-29 11:05:33 +0000

    chore: allow CORS

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 @app.get("/api/title")
 def read_root():
